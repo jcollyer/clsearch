091e1947ab299f0d54fc19731d8db638b954d1dc
jcollyer <collyerdesign@gmail.com>
2014-05-31 10:38:15 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clsearch::Application.config.secret_key_base = 'ada41aad7a25e4c6fb8472f51367b505479858dfb15ed994ea393fd3fee0e53f4d2effb5321d4ed477358fa23499e0c2c3d6ab115b2e751e939ac8ecd630d113'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clsearch::Application.config.session_store :cookie_store, key: '_clsearch_session'
