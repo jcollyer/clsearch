60c0377b6fa3c9dc5d308f2da67b4dd997529f3c
jcollyer <collyerdesign@gmail.com>
2014-05-31 21:11:16 -0700

    mess up stuff

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,25 @@
 
   // Parses returned response and extracts
   // the title, links, and text of each news story.
-  function top_stories(o){
-    var items = o.query.results.RDF.item;
-    var output = '';
-    var no_items=items.length;
-    for(var i=0;i<no_items;i++){
-      var title = items[i].title;
-      var link = items[i].link;
-      var desc = items[i].description;
-      output += "<h3><a href='" + link + "'>"+title+"</a></h3>" + desc + "<hr/>";
+    function get_items(o){
+      var items = o.query.results.RDF.item;
+      var output = '';
+      var number_of_items=items.length;
+      for(var i=0;i<number_of_items;i++){
+        var title = items[i].title;
+        var link = items[i].link;
+        var desc = items[i].description;
+        output += "<h3><a href='" + link + "'>link</a>"+title+"</h3>" + desc + "<hr/>";
+      }
+      // Place news stories in div tag
+      document.getElementById('results').innerHTML = output;
     }
-    // Place news stories in div tag
-    document.getElementById('results').innerHTML = output;
-  }
 
+$(document).ready(function(){
+  $("#query").click(function(){
+    var query = "bike"
+    var query_code = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search%20where%20location%3D%22sfbay%22%20and%20type%3D%22sss%22%20and%20query%3D%22"+query+"%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=get_items"
+    $("#scrip_code").attr({ src: query_code });
+  });
+
+});
